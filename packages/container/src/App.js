97fcfc035d1bade047e83edabd479626a23cd6b8
Phonekham <phone.madridista@gmail.com>
2021-11-11 10:37:19 +0700

    update hi

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,8 +5,8 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
     <div>
-    <h1>Hi</h1>
-    <hr/>
+      <h1>Hi!!</h1>
+      <hr />
       <MarketingApp />
     </div>
   );
